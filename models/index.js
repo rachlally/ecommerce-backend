00404804491e53628f6c07b6e26c684a6bb147be
Dahlia <rlally89@gmail.com>
2022-11-03 17:25:43 -0700

    routes seeded, starting insomnia tests

--- models/index.js
+++ models/index.js
@@ -5,8 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
@@ -20,6 +28,7 @@ Tag.belongsToMany(Product, {
   // as: 'product_tags',
   foreignKey: 'tag_id',
 });
+
 module.exports = {
   Product,
   Category,
